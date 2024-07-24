2b5898619e6e188855a1d664d3e603e6e16309a8
desco1960 <descoenuezuneka@gmail.comgit config --global user.name desco1960git config --global user.email descoenuezuneka@gmail.com>
2024-07-24 12:02:55 +0100

    this is my first commit

---
+++ latest/src/Project/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+
+function Navbar() {
+  return (
+
+    <>
+<nav className='first-nav'></nav>
+
+
+ <div>
+      <nav className='navbar'>
+        <li>HOME</li>     
+        <li>FEATURES</li>
+        <li>COLLECTIONS</li>
+        <li>SHOP</li>
+        <li>ABOUT US</li>
+        <li className='contact'>CONTACT US</li>
+        <li className='sp' style={{color:'gold'}} >SPECIAL OFFERS</li>
+        <li>PURCHASE THEME</li>
+
+      </nav>
+    </div>
+
+
+    </>
+
+
+   
+  )
+}
+
+export default Navbar
